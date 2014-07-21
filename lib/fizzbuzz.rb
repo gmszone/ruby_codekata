9121e8766dc2198586eab2801a7887055d0cc527
Phodal Huang <h@phodal.com>
2014-07-21 11:32:16 +0800

    Add the fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,6 +2,8 @@ class Fizzbuzz
   def get_fizz(num)
     if num%3 == 0
       "fizz"
+    elsif num%5 == 0
+      "buzz"
     end
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,11 +3,19 @@ require 'fizzbuzz'
 describe Fizzbuzz do
   it "should return a fizz" do
     fizzbuzz = Fizzbuzz.new()
-    fizzbuzz.get_fizz(3).should eq("fizz")
+    results = fizzbuzz.get_fizz(3)
+    results.should eq("fizz")
   end
 
   it "should return a buzz" do
     fizzbuzz = Fizzbuzz.new()
-    fizzbuzz.get_fizz(5).should eq("buzz")
+    results = fizzbuzz.get_fizz(5)
+    results.should eq("buzz")
+  end
+
+  it "should return a fizzbuzz" do
+    fizzbuzz = Fizzbuzz.new()
+    results = fizzbuzz.get_fizz(15)
+    results.should eq("fizzbuzz")
   end
 end
