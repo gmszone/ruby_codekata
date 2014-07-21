a3cd9374269a162cf20846459aedeca6adfc878a
Phodal Huang <h@phodal.com>
2014-07-21 11:33:14 +0800

    add the fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,11 @@
 class Fizzbuzz
   def get_fizz(num)
-    if num%3 == 0
-      "fizz"
+    if num%15 == 0
+      "fizzbuzz"
     elsif num%5 == 0
       "buzz"
+    elsif num%3 == 0
+      "fizz"
     end
   end
 end
