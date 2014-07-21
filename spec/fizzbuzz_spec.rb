7dfa6c6ef2e9c8795d4119ececc8e3d1fed66bd0
Phodal Huang <h@phodal.com>
2014-07-21 11:36:39 +0800

    Add the rspec config

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,21 +1,27 @@
 require 'fizzbuzz'
 
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
+end
+
 describe Fizzbuzz do
   it "should return a fizz" do
     fizzbuzz = Fizzbuzz.new()
     results = fizzbuzz.get_fizz(3)
-    results.should eq("fizz")
+    expect(results).to eq("fizz")
   end
 
   it "should return a buzz" do
     fizzbuzz = Fizzbuzz.new()
     results = fizzbuzz.get_fizz(5)
-    results.should eq("buzz")
+    expect(results).to eq("buzz")
   end
 
   it "should return a fizzbuzz" do
     fizzbuzz = Fizzbuzz.new()
     results = fizzbuzz.get_fizz(15)
-    results.should eq("fizzbuzz")
+    expect(results).to eq("fizzbuzz")
   end
 end
