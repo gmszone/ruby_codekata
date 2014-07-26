fc1f3c6c35409e0c039781c7246dbf8d6b9c66d7
Phodal Huang <h@phodal.com>
2014-07-26 15:45:37 +0800

    Add the method

--- class2/lib/palindromes.rb
+++ class2/lib/palindromes.rb
@@ -1,6 +1,13 @@
 class Fixnum
-  def palindrome_below(num)
-    [1,3]
+  def palindrome_below(base)
+    palindrome_match = []
+    num = self - 1
+    (1..num).each do |i|
+      if i.to_s(base) == i.to_s(base).reverse
+        palindrome_match << i
+      end
+    end
+    palindrome_match
   end
 end
 
--- class2/spec/palindromes_spec.rb
+++ class2/spec/palindromes_spec.rb
@@ -1,4 +1,5 @@
-require 'palindromes'
+require 'lib/palindromes'
+require 'rspec'
 
 RSpec.configure do |config|
   config.color = true
@@ -7,9 +8,13 @@ RSpec.configure do |config|
 end
 
 describe Fixnum do
-  it "should return a 1,3" do
+  it 'should return a 1,3' do
     results=[1,3]
     expect(5.palindrome_below(2)).to eq(results)
   end
 
+  it 'should return a 1,3,5,7' do
+    result2=[1,3,5,7]
+    expect(8.palindrome_below(2)).to eq(result2)
+  end
 end
