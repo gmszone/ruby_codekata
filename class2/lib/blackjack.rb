ffea085f12a9317aabee72b76d5ab9f4ad542ae7
Phodal Huang <h@phodal.com>
2014-07-26 16:47:23 +0800

    add the spec

--- class2/lib/blackjack.rb
+++ class2/lib/blackjack.rb
@@ -1,22 +1,8 @@
-def scoreHand(params)
-  result = 0
-  h = {'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 1}
-  params.each do |each_num|
-    if h[each_num]== nil
-      result += each_num.to_i
-    else
-      result += h[each_num].to_i
-    end
+class Blackjack
+  def scoreHand(params)
+    result = 0
+    h = {'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 1}
+    params.each{ |each_num| h[each_num]== nil ? result += each_num.to_i : result += h[each_num].to_i }
+    (params.include?('A') && result <= 11) ? result + 10 : result
   end
-  if params.include?('A') && result <= 11
-    result =  result + 10
-  end
-  p result
-  result
 end
-
-scoreHand(%w(A))
-scoreHand(%w(A J))
-scoreHand(%w(A 10 A))
-scoreHand(%w(5 3 7))
-scoreHand(%w(5 4 3 2 A K))
