59e7477415b8d1d2c67cea4830cd2c323c610381
Phodal Huang <h@phodal.com>
2014-07-26 15:46:48 +0800

    refactor the method again

--- class2/lib/palindromes.rb
+++ class2/lib/palindromes.rb
@@ -1,7 +1,6 @@
 class Fixnum
   def palindrome_below(base)
-    num = self - 1
-    (1..num).select{ |i| i.to_s(base) == i.to_s(base).reverse }
+    (1..self - 1).select{ |i| i.to_s(base) == i.to_s(base).reverse }
   end
 end
 
