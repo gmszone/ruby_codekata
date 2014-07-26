b283f11c8cbc10a3434de468388045b4dc9fe19a
Phodal Huang <h@phodal.com>
2014-07-26 15:46:14 +0800

    refactor the method

--- class2/lib/palindromes.rb
+++ class2/lib/palindromes.rb
@@ -1,13 +1,7 @@
 class Fixnum
   def palindrome_below(base)
-    palindrome_match = []
     num = self - 1
-    (1..num).each do |i|
-      if i.to_s(base) == i.to_s(base).reverse
-        palindrome_match << i
-      end
-    end
-    palindrome_match
+    (1..num).select{ |i| i.to_s(base) == i.to_s(base).reverse }
   end
 end
 
