44c1c176e65997bc8f301981037552515547b8bb
Phodal Huang <h@phodal.com>
2014-07-27 15:18:19 +0800

    Refactor the balckjack

--- class2/lib/blackjack.rb
+++ class2/lib/blackjack.rb
@@ -1,8 +1,6 @@
-class Blackjack
-  def scoreHand(params)
-    result = 0
-    h = {'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 1}
-    params.each{ |each_num| h[each_num]== nil ? result += each_num.to_i : result += h[each_num].to_i }
-    (params.include?('A') && result <= 11) ? result + 10 : result
-  end
+def scoreHand(params)
+  result = 0
+  h = {'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 1}
+  params.each{ |each_num| h[each_num]== nil ? result += each_num.to_i : result += h[each_num].to_i }
+  (params.include?('A') && result <= 11) ? result + 10 : result
 end
