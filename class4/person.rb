986a83e4727b6ec63325b93cee2b2fdc862f58b9
Phodal Huang <h@phodal.com>
2014-08-11 09:17:46 +0800

    Add the class4

---
+++ class4/person.rb
@@ -0,0 +1,21 @@
+class Person
+  include Comparable
+
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+
+  def <=> person
+    @name <=> person.name
+  end
+
+  def to_s
+    "#{name}"
+  end
+end
+
+p1 = Person.new("Matz")
+p2 = Person.new("Guido")
+p3 = Person.new("Larry")
+puts [p1, p2, p3].sort
